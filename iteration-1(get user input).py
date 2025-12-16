a015b63a398c12a0cbd1621cfe1fb436c54501db
Xavi-P <Xman08711@gmail.com>
2025-12-16 12:16:06 -0500

    Create iteration-1(get user input).py

---
+++ iteration-1(get user input).py
@@ -0,0 +1,12 @@
+decision=""
+def get_user_choice(decision):
+  decision=input("rock,paper, or scissors?:")
+  while decision != "rock" and decision != "paper" and decision != "scissors":
+    print("invalid input please enter rock, paper, or scissors to continue.")
+    decision=input("rock,paper, or scissors?:")
+  return decision
+
+  
+decision=get_user_choice(decision)
+# to test
+print(decision)
