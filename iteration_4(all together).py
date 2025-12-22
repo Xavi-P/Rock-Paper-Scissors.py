ba0c7cd3ec9f47bd2c4fd58f5b652e4c2d5807bd
Xavi-P <Xman08711@gmail.com>
2025-12-22 12:04:41 -0500

    Create iteration_4(all together).py

---
+++ iteration_4(all together).py
@@ -0,0 +1,40 @@
+import random
+
+decision=""
+def get_user_choice(decision):
+  decision=input("rock,paper, or scissors?:")
+  while decision != "rock" and decision != "paper" and decision != "scissors":
+    print("invalid input please enter rock, paper, or scissors to continue.")
+    decision=input("rock,paper, or scissors?:")
+  return decision
+
+  
+
+
+def get_computer_choice():
+  choises = ["rock", "paper", "scissors"]
+  # to test
+  commputer_rand_choise=random.choice(choises)
+  return commputer_rand_choise
+get_computer_choice()
+
+
+
+player_hand=get_user_choice(decision)
+computer_hand=get_computer_choice()
+
+# to test
+print("player:"+player_hand)
+print("computer:"+computer_hand)
+
+def results(player_hand,computer_hand):
+  # if player_hand=="rock":
+  #   print("fart")
+  if player_hand==computer_hand:
+    print("it a tie")
+  elif player_hand=="rock" and computer_hand=="paper" or player_hand=="paper" and computer_hand=="scissors" or player_hand=="scissors" and computer_hand=="rock":
+      print("you lose")
+  else:
+      print("you win")
+   
+results(player_hand,computer_hand)
